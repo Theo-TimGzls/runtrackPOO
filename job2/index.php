dbc5ea097d97aabb38e0647343a852d917d38d74
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-02-03 14:50:24 +0100

    suppr public de function

--- job2/index.php
+++ job2/index.php
@@ -5,13 +5,13 @@ class Operation
     public int $nombre1;
     public int $nombre2;
 
-    public function __construct(?int $nombre1 = 1, ?int $nombre2 = 2)
+    function __construct(?int $nombre1 = 1, ?int $nombre2 = 2)
     {
         $this->nombre1 = $nombre1;
         $this->nombre2 = $nombre2;
     }
 
-    public function calcule(): string
+    function calcule(): string
     {
         $resultat = "La somme de $this->nombre1 et $this->nombre2 est: " . ($this->nombre1 + $this->nombre2);
         return $resultat;
