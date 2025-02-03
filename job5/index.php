bd35b193fb1a1c5ba9d6a9610e755ed875690c6c
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-02-03 11:56:38 +0100

    ajout: Classe 'Animal' avec differents attributs init vide dans son constructeur, creation d'une méthode 'vieillir' qui ajoute 1 à l'âge et enfin creation d'une méthode 'nommer()' qui prend en paramètre le nom de l'animal

---
+++ job5/index.php
@@ -0,0 +1,35 @@
+<?php
+
+class Animal
+{
+    public string $age;
+    public string $prenom;
+
+    public function __construct(?string $age = "0", ?string $prenom = "")
+    {
+        $this->age = $age;
+        $this->nom = $prenom;
+    }
+
+    public function vieillir(): string
+    {
+        $resultat = "L'âge de l'animal est " . ($this->age + 1) . " ans";
+        return $resultat;
+    }
+
+    public function nommer(): string
+    {
+        $resultat = "Le nom de l'animal est $this->prenom";
+        return $resultat;
+    }
+}
+
+$animal1 = new Animal("5", "Rantanplan");
+$animal2 = new Animal("10", "Felix");
+
+echo $animal1->vieillir() . "<br>";
+echo $animal2->vieillir() . "<br>";
+echo $animal1->nommer() . "<br>";
+echo $animal2->nommer() . "<br>";
+
+?>
