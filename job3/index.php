d63518bb0c4952031d48681326c21853c151e9d4
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-02-03 10:54:41 +0100

    ajout; creation d'une classe 'Personne' avec des attributs, ajout d'une methode 'SePresenter' qui retourne les attributs de la personne, ajout d'un contructeur prenant en paramètres de quoi donner des valeurs aux attrbuts

---
+++ job3/index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Personne
+{
+    public string $nom;
+    public string $prenom;
+
+    public function __construct(?string $nom = "", ?string $prenom = "")
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+
+    public function SePresenter(): string
+    {
+        $resultat = "Je suis $this->prenom $this->nom";
+        return $resultat;
+    }
+}
+
+$personne1 = new Personne("Doe", "John");
+$personne2 = new Personne("Dupond", "Jean");
+
+echo $personne1->SePresenter() . "<br>";
+echo $personne2->SePresenter() . "<br>";
+?>
